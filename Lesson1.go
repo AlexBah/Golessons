f3c92e3c58a782d2a3d76146b14f481dbe85e176
Alex Bah <P_osts@mail.ru>
2024-04-20 13:36:55 +0700

    Commit#4

--- Lesson1.go
+++ Lesson1.go
@@ -4,13 +4,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
+	start := time.Now()
+	s, sep := "", ""
+	for i := 0; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
+	fmt.Printf("%.3fs elapsed\n", time.Since(start).Seconds())
+
+	start = time.Now()
+	for i, arg := range os.Args[0:] {
+		s = strconv.Itoa(i) + " " + arg
+		fmt.Println(s)
+	}
+	fmt.Printf("%.3fs elapsed\n", time.Since(start).Seconds())
+
+	start = time.Now()
+	fmt.Println(strings.Join(os.Args[0:], " "))
+	fmt.Printf("%.3fs elapsed\n", time.Since(start).Seconds())
 }
