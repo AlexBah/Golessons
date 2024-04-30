5a3609db0633ed4778101c30a08acc5625f1dc75
Alex Bah <P_osts@mail.ru>
2024-04-30 18:56:51 +0700

    commit#8

--- Lesson1.go
+++ Lesson1.go
@@ -8,9 +8,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	stringInFile := make(map[string]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countsLines(os.Stdin, counts)
+		countsLines(os.Stdin, counts, stringInFile)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -18,20 +19,29 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countsLines(f, counts)
+			countsLines(f, counts, stringInFile)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%s\n", stringInFile[line])
+			//			for line2, line3 := range stringInFile {
+			//				if line == line3 {
+			//					fmt.Printf("%s\t%s\n", line, line2)
+			//				}
+			//			}
+			fmt.Printf("----------------\n")
 		}
 	}
 }
 
-func countsLines(f *os.File, counts map[string]int) {
+func countsLines(f *os.File, counts map[string]int, stringInFile map[string]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		line := input.Text()
+		counts[line]++
+		stringInFile[line] = stringInFile[line] + f.Name() + " "
 	}
 }
