d863f38a29e689efb565c964e6f27ab8c0ea455d
Alex Bah <P_osts@mail.ru>
2024-05-01 22:12:40 +0700

    commit12

--- Lesson1.go
+++ Lesson1.go
@@ -2,68 +2,33 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/gif"
+	"fmt"
 	"io"
-	"log"
-	"math"
-	"math/rand"
 	"net/http"
 	"os"
-	"time"
-)
-
-var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
-
-const (
-	backgroundIndex = 1 // first color in palette
-	foregroundIndex = 0 // next color in palette
 )
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	if len(os.Args) > 1 && os.Args[1] == "web" {
-		handler := func(w http.ResponseWriter, r *http.Request) {
-			lissajous(w)
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
 		}
-		http.HandleFunc("/", handler)
-		log.Fatal(http.ListenAndServe("localhost:8000", nil))
-		return
-	}
-	if len(os.Args) > 1 && os.Args[1] == "file" {
-		gf, _ := os.Create("out.gif")
-		lissajous(gf)
-		gf.Close()
-		return
-	}
-	lissajous(os.Stdout)
-}
-
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
-	freq := rand.Float64() * 3 // relative frequency of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				foregroundIndex)
+		bytes, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+			fmt.Printf("%d\n", bytes)
+			os.Exit(1)
 		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+
+		//		b, err := ioutil.ReadAll(resp.Body)
+		//		resp.Body.Close()
+		//		if err != nil {
+		//			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+		//			os.Exit(1)
+		//		}
+		//		fmt.Printf("%s", b)
 	}
-	gif.EncodeAll(out, &anim) // Note: ignoring encoding errors
 }
