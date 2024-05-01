f4bd81cc2f48a5b57977f8407312cf5b5f7aaf0b
Alex Bah <P_osts@mail.ru>
2024-05-01 16:23:42 +0700

    commit#11

--- Lesson1.go
+++ Lesson1.go
@@ -1,42 +1,69 @@
+// Lissajous generates GIF animations of rantom Lissajous figures.
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
 	"os"
+	"time"
+)
+
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
+
+const (
+	backgroundIndex = 1 // first color in palette
+	foregroundIndex = 0 // next color in palette
 )
 
 func main() {
-	counts := make(map[string]int)
-	stringInFile := make(map[string]string)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countsLines(os.Stdin, counts, stringInFile)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countsLines(f, counts, stringInFile)
-			f.Close()
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			lissajous(w)
 		}
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-			fmt.Printf("%s\n", stringInFile[line])
-			fmt.Printf("----------------\n")
-		}
+	if len(os.Args) > 1 && os.Args[1] == "file" {
+		gf, _ := os.Create("out.gif")
+		lissajous(gf)
+		gf.Close()
+		return
 	}
+	lissajous(os.Stdout)
 }
 
-func countsLines(f *os.File, counts map[string]int, stringInFile map[string]string) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		line := input.Text()
-		counts[line]++
-		stringInFile[line] = stringInFile[line] + f.Name() + " "
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				foregroundIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
+	gif.EncodeAll(out, &anim) // Note: ignoring encoding errors
 }
