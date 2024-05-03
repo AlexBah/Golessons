7d2439e5bbd44f67f05c761d44b11e113481b22a
Alex Bah <P_osts@mail.ru>
2024-05-03 22:55:27 +0700

    commit15

--- Lesson1.go
+++ Lesson1.go
@@ -1,39 +1,39 @@
-// Lissajous generates GIF animations of rantom Lissajous figures.
 package main
 
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+	ch := make(chan string)
 	for _, url := range os.Args[1:] {
-		if !strings.HasPrefix(url, "http://") {
-			url = "http://" + url
-		}
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s\n", resp.Status)
-		bytes, err := io.Copy(os.Stdout, resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
-			fmt.Printf("%d\n", bytes)
-			os.Exit(1)
-		}
-
-		//		b, err := ioutil.ReadAll(resp.Body)
-		//		resp.Body.Close()
-		//		if err != nil {
-		//			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
-		//			os.Exit(1)
-		//		}
-		//		fmt.Printf("%s", b)
+		go fetch(url, ch)
 	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }
