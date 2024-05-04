43dd02e392d9f73568566d56ad90657552fe1246
Alex Bah <P_osts@mail.ru>
2024-05-04 23:35:52 +0700

    commit18

--- Lesson1.go
+++ Lesson1.go
@@ -2,15 +2,83 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
+	"os"
+	"sync"
 )
+
+var mu sync.Mutex
+var count int
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	http.HandleFunc("/gif", handlerGif)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count = %d\n", count)
+	mu.Unlock()
+}
+
+func handlerGif(w http.ResponseWriter, r *http.Request) {
+	//lissajous(w)
+	gf, _ := os.Create("out.gif")
+	lissajous(gf)
+	gf.Close()
+}
+
+func lissajous(out io.Writer) {
+	var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
+
+	const (
+		backgroundIndex = 1     // first color in palette
+		foregroundIndex = 0     // next color in palette
+		cycles          = 5     // number of complete x oscillator revolutions
+		res             = 0.001 // angular resolution
+		size            = 100   // image canvas covers [-size..+size]
+		nframes         = 64    // number of animation frames
+		delay           = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				foregroundIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // Note: ignoring encoding errors
 }
