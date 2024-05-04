17c5a2c4cceb0b61037c4edf5d0e610dc4a9be5a
Alex Bah <P_osts@mail.ru>
2024-05-04 17:28:00 +0700

    commit17

--- Lesson1.go
+++ Lesson1.go
@@ -2,40 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
-	"time"
 )
 
 func main() {
-	start := time.Now()
-	ch := make(chan string)
-	for _, url := range os.Args[1:] {
-		go fetch(url, ch)
-	}
-	gf, _ := os.Create("out.log")
-	for range os.Args[1:] {
-		fmt.Fprintln(gf, <-ch)
-	}
-	fmt.Fprintf(gf, "%.2fs elapsed\n", time.Since(start).Seconds())
-	gf.Close()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func fetch(url string, ch chan<- string) {
-	start := time.Now()
-	resp, err := http.Get(url)
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
-	}
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
-		return
-	}
-	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
